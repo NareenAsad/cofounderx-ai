329154a568e613b8b29943a263c1b657bce3ec1f
NareenAsad <bennynanny80@gmail.com>
2025-11-21 22:32:08 +0500

    Clean repo: remove tracked ignored files

--- .next/dev/server/chunks/ssr/7aa4c__next-internal_server_app_dashboard_collaboration_page_actions_e268c438.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Downloads/co-founder-x/.next-internal/server/app/dashboard/collaboration/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=7aa4c__next-internal_server_app_dashboard_collaboration_page_actions_e268c438.js.map
--- .next/dev/server/chunks/ssr/Downloads_co-founder-x__next-internal_server_app_page_actions_085a9bd1.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Downloads/co-founder-x/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=Downloads_co-founder-x__next-internal_server_app_page_actions_085a9bd1.js.map
